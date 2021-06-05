38fdaf9e5782a162703667fb344d55f9aa237f8e
Marc Valenti <marcvalenti@bain.com>
2021-06-05 17:21:37 +0200

    improved data importing

--- src/01_data_importing.R
+++ src/01_data_importing.R
@@ -8,11 +8,17 @@ data_source <- 'http://s3.kiva.org/snapshots/kiva_ds_csv.zip'
 print('***************************************************')
 print('****************  Downloading data ****************')
 
-system(glue('curl {data_source} -O'))
+system('mkdir data')
+system('mkdir data/raw ')
+
+system(glue('cd data/raw && curl -O {data_source}'))
+system(glue('cd data/raw && unzip kiva_ds_csv.zip'))
 
 print('***************************************************')
 print('**********  Data has been downloaded **************')
 print('***************************************************')
+
+system(glue('ls -l data/raw'))
 
 ###########################################################
 ################  Data subsetting   #######################
@@ -22,6 +28,9 @@ print('***************************************************')
 loans <- fread('data/raw/loans.csv')
 colnames(loans) <- unlist(lapply(names(loans), function(x) str_to_lower(x)))
 loans$funded <- loans$funded_amount==loans$loan_amount
+loans$time_to_fund <- as.numeric(as.POSIXct(loans$raised_time)-as.POSIXct(loans$posted_time))
+
+date_subset <- '2020-11-16'
   
 df <- loans %>%
   # Country is Philippines
@@ -30,7 +39,7 @@ df <- loans %>%
          # Partner Id is 145
          ) %>%         
   # Posted Date is after 2016-03-01
-  filter(to_date(posted_time)=='2021-02-26') %>%
+  filter(to_date(posted_time)==date_subset) %>%
   filter(borrower_genders=="female",  
          # There is only ONE borrower IN THE PICTURE
          borrower_pictured=="true",   
@@ -41,8 +50,9 @@ df <- loans %>%
          # The sector Name is either Agriculture, Food or Retail
          sector_name%in%c("Agriculture","Food","Retail")) 
 
-data.table::fwrite(df, 'data/processed/loans_subset.csv')
+data.table::fwrite(df, glue::glue('data/processed/loans_subset_{date_subset}.csv'))
 
 print('***************************************************')
-print('*** Created  data/processed/loans_subset.csv ******')
+print(glue::glue('*** Created  data/processed/loans_subset_{date_subset}.csv ******'))
 print('***************************************************')
+
